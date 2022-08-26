2549dc559448019347b849d85e498101dc1343d2
Gabriele Marcato <gabriele@kuama.net>
2022-08-26 18:00:58 +0200

    wip

--- Example/kMusicSwift/ViewController.swift
+++ Example/kMusicSwift/ViewController.swift
@@ -6,12 +6,18 @@
 //  Copyright (c) 2022 cc18675dd6a3d8f9dd752f1bc3bc9210bda2938e. All rights reserved.
 //
 
+import AVFAudio
+import kMusicSwift
 import UIKit
 
+@available(iOS 15.0, *)
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let a = JustAudioPlayer.init()
+        a.play(trackPath: "nature")
+        
     }
 
     override func didReceiveMemoryWarning() {
