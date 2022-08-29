565bb9f59bf75f5420d797528707cc07bbe19804
Daniele De Matteo <daniele@kuama.net>
2022-08-29 09:48:48 +0200

    track resource from asset

--- kMusicSwift/Classes/TrackResource.swift
+++ kMusicSwift/Classes/TrackResource.swift
@@ -5,8 +5,51 @@
 //  Created by Mac on 26/08/22.
 //
 
+import AVFAudio
+
 // URLs
 // Assets
 // Files
 // TO BE SUPPORTED Schemes: https | file: | asset:
-public struct TrackResource {}
+public struct TrackResource {
+    var audioFile: AVAudioFile
+
+    init(audioFile: AVAudioFile) {
+        self.audioFile = audioFile
+    }
+}
+
+/// Builds a TrackResource
+public class TrackResourceBuilder {
+    private var inputFileUrl: URL?
+
+    public init(inputFileUrl: URL? = nil) {
+        self.inputFileUrl = inputFileUrl
+    }
+
+    /// Expects an asset name with extension. Ex "track.mp3"
+    @discardableResult
+    public func fromAsset(_ asset: String) throws -> TrackResourceBuilder {
+        guard let inputFileUrl = Bundle.main.url(forResource: asset, withExtension: "") else {
+            throw JustAudioPlayerError.couldNotFindAsset
+        }
+
+        self.inputFileUrl = inputFileUrl
+
+        return self
+    }
+
+    public func build() throws -> TrackResource {
+        guard let inputFileUrl = inputFileUrl else {
+            // TODO: this should become a "resource not provided to builder error" when all resource types are handled
+            fatalError("Provide an asset via the fromAsset method")
+        }
+
+        // Load the track inside a AVAudioFile
+        guard let inputFile = try? AVAudioFile(forReading: inputFileUrl) else {
+            throw JustAudioPlayerError.couldNotLoadUrlIntoTrackResource
+        }
+
+        return TrackResource(audioFile: inputFile)
+    }
+}
