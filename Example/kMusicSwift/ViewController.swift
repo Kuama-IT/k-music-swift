e6ff85e7e3052956e0b26da5fd26f4822f0227ea
Daniele De Matteo <daniele@kuama.net>
2022-08-29 09:48:48 +0200

    track resource from asset

--- Example/kMusicSwift/ViewController.swift
+++ Example/kMusicSwift/ViewController.swift
@@ -6,22 +6,36 @@
 //  Copyright (c) 2022 cc18675dd6a3d8f9dd752f1bc3bc9210bda2938e. All rights reserved.
 //
 
-import AVFAudio
+import AVFoundation
 import kMusicSwift
 import UIKit
 
 @available(iOS 15.0, *)
 class ViewController: UIViewController {
+    let jap = JustAudioPlayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let a = JustAudioPlayer.init()
-        a.play(trackPath: "nature")
-        
+
+        do {
+            let builder = try TrackResourceBuilder()
+                .fromAsset("AudioSource.mp3")
+            let resource = try builder.build()
+            jap.setTrack(resource)
+        } catch JustAudioPlayerError.couldNotFindAsset {
+            print("couldNotFindAsset")
+        } catch JustAudioPlayerError.couldNotLoadUrlIntoTrackResource {
+            print("couldNotLoadUrlIntoTrackResource")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    @IBAction func onPlay(_: Any) {
+        jap.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
--- kMusicSwift/Classes/JustAudioPlayer.swift
+++ kMusicSwift/Classes/JustAudioPlayer.swift
@@ -33,27 +33,26 @@ public enum LoopMode {
 public class JustAudioPlayer {
     private let engine: AVAudioEngine = .init()
     private let playerNode: AVAudioPlayerNode = .init()
+
     // To be forwarded to the http client, in case we load a track from the internet
     var httpHeaders: [String: String] = [:]
+
+    private var queue: [TrackResource] = []
 
     public init() {}
 
     /// To be modified in order to handle multiple tracks at once
-    public func setTrack(_: TrackResource) {}
+    public func setTrack(_ track: TrackResource) {
+        queue.append(track)
+    }
 
     public func setAudioSource(_: AudioSource) {}
 
     // Play without waiting for completion
     // Play while waiting for completion
-    public func play(trackPath: String) {
-        // Read track URL from the bundle assets
-        guard let inputFileUrl = Bundle.main.url(forResource: trackPath, withExtension: "mp3") else {
-            fatalError("could not load a track")
-        }
-        // Load the track inside a AVAudioFile
-        guard let inputFile = try? AVAudioFile(forReading: inputFileUrl) else {
-            fatalError("input file fail")
-        }
+    public func play() {
+        // TODO: ensure we have something in queue or return
+        let inputFile = queue.first!.audioFile
 
         // Setup the engine:
 
@@ -72,46 +71,13 @@ public class JustAudioPlayer {
 
         playerNode.scheduleFile(inputFile, at: nil)
 
-        // Prepare the output file
-        guard let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            fatalError("document directory fail")
-        }
-
-        let outputFileUrl = documentsDirectoryURL.appendingPathComponent(Date.now.ISO8601Format())
-
-        // Handy while testing to retrieve quickly the file inside the OS filesystem
-        print(outputFileUrl.absoluteString)
-
-        // We need some settings for the output audio file. The quickiest way to test this is to grab the same settings of the output node of the engine.
-        // Sadly it defaults to WAV format for the output file, and since we're planning to upload this file to the server, is the less performant format
-        // Some work should be done to extrapolate a good settings configuration (see final comment)
-        let settings = engine.outputNode.outputFormat(forBus: 0).settings
-
-        guard let outputFile = try? AVAudioFile(forWriting: outputFileUrl, settings: settings) else {
-            fatalError("document directory fail")
-        }
-
-        // Write engine final output to file:
-        // outputNode will never trigget the tap 🤷‍♂️ (probably because we didn't connect anything), but since we can connect player -> effects -> mainMixerNode
-        // tapping the last node is more than enough for a poc 🥷.
-        engine.mainMixerNode.installTap(onBus: 0, bufferSize: 4096, format: nil) { buffer, _ in
-            do {
-                try outputFile.write(from: buffer)
-            } catch {
-                // We cannot re-throw here due to the signature of the block
-                print("Error: \(error)")
-                print("Description: \(error.localizedDescription)")
-            }
-        }
-
         playerNode.play()
     }
 
     // Pause but remain ready to play
     public func pause() {
         playerNode.pause()
-        engine.pause() 
-        
+        engine.pause()
     }
 
     public func stop() {
