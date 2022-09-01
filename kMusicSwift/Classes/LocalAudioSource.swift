c754e5054a5bf205f80e9c8cd721ff595cf84068
Daniele De Matteo <daniele@kuama.net>
2022-09-01 16:04:45 +0200

    scaffold audio source types

---
+++ kMusicSwift/Classes/LocalAudioSource.swift
@@ -0,0 +1,12 @@
+//
+// LocalAudioSource.swift
+// kMusicSwift
+// Created by Kuama Dev Team on 01/09/22
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+
+/**
+ An `AudioSource` that holds an audio file stored inside the local filesystem
+ */
+public class LocalAudioSource: IndexedAudioSource {}
---
+++ kMusicSwift/Classes/RemoteAudioSource.swift
@@ -0,0 +1,12 @@
+//
+// RemoteAudioSource.swift
+// kMusicSwift
+// Created by Kuama Dev Team on 01/09/22
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+
+/**
+ An `AudioSource` the holds a single audio stream
+ */
+public class RemoteAudioSource: IndexedAudioSource {}
