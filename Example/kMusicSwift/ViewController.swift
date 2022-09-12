3f642268561c652bfcde76ecd07c9724c816e32d
Gabriele Marcato <gabriele@kuama.net>
2022-09-12 17:54:06 +0200

    feat: multiple audio tracks playing simultaneously

--- Example/kMusicSwift/ViewController.swift
+++ Example/kMusicSwift/ViewController.swift
@@ -13,7 +13,9 @@ import UIKit
 
 @available(iOS 15.0, *)
 class ViewController: UIViewController {
-    private lazy var jap = JustAudioPlayer()
+    private lazy var engine = AVAudioEngine()
+    private lazy var jap = JustAudioPlayer(engine: engine)
+    private lazy var jap2 = JustAudioPlayer(engine: engine)
     @IBOutlet var playOrPauseBtn: UIButton!
     @IBOutlet var stopBtn: UIButton!
     @IBOutlet var loopModeBtn: UIButton!
@@ -137,13 +139,14 @@ class ViewController: UIViewController {
         distortion.setBypass(false)
         distortion.setWetDryMix(40)
 
-        _ = RemoteAudioSource(at: "https://www.fesliyanstudios.com/musicfiles/2019-04-23_-_Trusted_Advertising_-" +
+        let remote = RemoteAudioSource(at: "https://www.fesliyanstudios.com/musicfiles/2019-04-23_-_Trusted_Advertising_-" +
             "_www.fesliyanstudios.com/15SecVersion2019-04-23_-_Trusted_Advertising_-_www.fesliyanstudios.com.mp3",
-            effects: [delay])
+            effects: [])
 //        let local = LocalAudioSource(at: "AudioSource.mp3", effects: [delay])
-        let local = LocalAudioSource(at: "AudioSource.mp3", effects: [reverb])
+//        let local = LocalAudioSource(at: "AudioSource.mp3", effects: [reverb])
+        let shakerando = LocalAudioSource(at: "shakerando.mp3")
 //        let local = LocalAudioSource(at: "AudioSource.mp3", effects: [distortion])
-//        let local = LocalAudioSource(at: "AudioSource.mp3", effects: [])
+        let local = LocalAudioSource(at: "AudioSource.mp3", effects: [])
         do {
             let eq = try Equalizer(preSets: [
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -152,18 +155,96 @@ class ViewController: UIViewController {
                 [5, 4, 3.5, 3, 1, 0, 0, 0, 0, 0],
             ])
             try jap.setEqualizer(eq)
-            /*
-             _ = try ClippingAudioSource(with: local, from: 10.0, to: 15.0)
+            _ = try ClippingAudioSource(with: local, from: 10.0, to: 15.0)
 
-             try jap.writeOutputToFile()*/
+            try jap.writeOutputToFile()
 
-            jap.addAudioSource(IndexedAudioSequence(with: local))
+            jap.addAudioSource(IndexedAudioSequence(with: remote))
             try jap.setVolume(0.1)
             jap.setLoopMode(.off)
             try jap.play()
+
+            jap2.addAudioSource(IndexedAudioSequence(with: shakerando))
+            try jap2.setVolume(0.2)
+            try jap2.play()
+
         } catch {
             handleError(error: error)
         }
+
+        jap2Streams()
+    }
+
+    private func jap2Streams() {
+        jap2.$speed
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 SPEED")
+            }).store(in: &cancellables)
+
+        jap2.$duration
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 DURATION")
+            }).store(in: &cancellables)
+
+        jap2.$queueIndex
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 QUEUE INDEX")
+            }).store(in: &cancellables)
+
+        jap2.$elapsedTime
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 ELAPSED TIME")
+//                if $0 > 5{
+//                    self.jap2.pause()
+//                }
+            }).store(in: &cancellables)
+
+        jap2.$loopMode
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 LOOP MODE")
+            }).store(in: &cancellables)
+
+        jap2.$volume
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 VOLUME")
+            }).store(in: &cancellables)
+
+        jap2.isShuffling
+            .compactMap { $0 }.receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 IS SHUFFLING")
+            }).store(in: &cancellables)
+
+        jap2.$isPlaying
+            .compactMap { $0 }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 IS PLAYING")
+            }).store(in: &cancellables)
+
+        jap2.$outputWriteError
+            .compactMap { $0 }.receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 OUTPUT WRITE ERROR")
+            }).store(in: &cancellables)
+
+        jap2.$outputAbsolutePath
+            .compactMap { $0 }.receive(on: DispatchQueue.main)
+            .sink(receiveValue: {
+                print("\($0) JAP2 ABSOLUTE PATH")
+            }).store(in: &cancellables)
     }
 
     @IBAction func onPlayOrPause(sender _: UIButton) {
